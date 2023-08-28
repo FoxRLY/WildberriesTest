da5ddfaefa7863256c716810e932c0e3fbe8fe21
FoxRLY <nevik0016@gmail.com>
2023-08-28 23:34:34 +0300

    Added start sequence for project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use wildberries_test::server::Server; 
+
+#[actix_web::main]
+async fn main() {
+    dotenv().ok();
+    let app = Server::builder()
+        .host("0.0.0.0".to_owned())
+        .port(8080)
+        .build();
+    app.start().await.unwrap();
 }
